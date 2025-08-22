bb2c9b0944f05799d75a0feab8b803f2dd1de7c2
Chirag <chirag.gupta2992@gmail.com>
2025-08-21 23:38:38 -0400

    Added ValueArray for storing literals (for now only doubles)

---
+++ value.c
@@ -0,0 +1,23 @@
+#include "value.h"
+#include "memory.h"
+
+void initValueArray(ValueArray *array) {
+    array->capacity = 0;
+    array->count = 0;
+    array->values = NULL;
+}
+
+void writeValueArray(ValueArray *array, Value value) {
+    if (array->count = array->capacity) {
+        int newCapacity = GROW_CAPACITY(array->capacity);
+        array->values = GROW_ARRAY(Value, array->values, array->capacity, newCapacity);
+        array->capacity = newCapacity;
+    }
+    array->values[array->count] = value;
+    array->count++;
+}
+
+void freeValueArray(ValueArray *array) {
+    FREE_ARRAY(Value, array, array->capacity);
+    initValueArray(array);
+}
